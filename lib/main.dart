d6a9343dfb10908c2cdef9efa6d9de8299f595c3
m-cheicki <mcheicki.dev@gmail.com>
2022-11-22 10:34:23 +0100

    📱 Basic responsive rules

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:insta_clone_flutter/responsive/mobile_screen_layout.dart';
+import 'package:insta_clone_flutter/responsive/responsive_layout_screen.dart';
+import 'package:insta_clone_flutter/responsive/web_screen_layout.dart';
+import 'package:insta_clone_flutter/utils/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,21 +15,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'InstaClone',
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: mobileBackgroundColor,
+        ),
+        home: const Scaffold(
+          body: ResponsiveLayout(
+            mobileScreenLayout: MobileScreenLayout(),
+            webScreenLayout: WebeScreenLayout(),
+          ),
+        ));
   }
 }
 
---
+++ lib/responsive/web_screen_layout.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class WebeScreenLayout extends StatelessWidget {
+  const WebeScreenLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Mobile layout'),
+      ),
+    );
+  }
+}
